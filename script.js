2b615cf326551dd527a47f7a80518b378b96dbc0
miho0 <112256554+miho0@users.noreply.github.com>
2024-03-09 14:24:20 +0100

    game results

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let state;
+let board;
+let turnPlayer = true;
 
 const init = () => {
-  state = [
-    [1, 2, 0],
-    [0, 2, 0],
-    [0, 2, 0],
+  board = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
   ];
   draw();
 };
@@ -13,7 +14,7 @@ const draw = () => {
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
       const elm = $(`.${i}${j}`);
-      switch (state[i][j]) {
+      switch (board[i][j]) {
         case 0:
           elm.html("");
           break;
@@ -31,3 +32,64 @@ const draw = () => {
 };
 
 init();
+
+const playerMove = (e) => {
+  const field = e.target.classList[0];
+  move(field);
+};
+
+const move = (field) => {
+  const x = field[0];
+  const y = field[1];
+  if (board[x][y] === 0) {
+    if (turnPlayer) board[x][y] = 1;
+    else board[x][y] = 2;
+    console.log(getResult());
+    draw();
+    turnPlayer = !turnPlayer;
+    if (!turnPlayer) AIMove();
+  }
+};
+
+const getResult = () => {
+  if (board[0][0] === board[0][1] && board[0][1] === board[0][2])
+    return board[0][0];
+  if (board[1][0] === board[1][1] && board[1][1] === board[1][2])
+    return board[1][0];
+  if (board[2][0] === board[2][1] && board[2][1] === board[2][2])
+    return board[2][0];
+
+  if (board[0][0] === board[1][0] && board[1][0] === board[2][0])
+    return board[0][0];
+  if (board[0][1] === board[1][1] && board[1][1] === board[2][1])
+    return board[0][1];
+  if (board[0][2] === board[1][2] && board[1][2] === board[2][2])
+    return board[0][2];
+
+  if (board[0][0] === board[1][1] && board[1][1] === board[2][2])
+    return board[0][0];
+  if (board[0][2] === board[1][1] && board[1][1] === board[2][0])
+    return board[0][2];
+
+  return 0;
+};
+
+const getAllPossibleMoves = () => {};
+
+const AIMove = () => {
+  if (!turnPlayer) {
+  }
+};
+
+const minimax = () => {
+  // dobimo vse možne poteze
+  // za vsako potezo hipotetično
+};
+
+$(document).ready(function () {
+  $(".field").click((e) => playerMove(e));
+});
+
+// heutistic function:
+// if win, 10
+// if lose, 0
